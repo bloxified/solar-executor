c46b1a70f65948eaee6252a592ac242b230f749c
bloxified <163031343+bloxified@users.noreply.github.com>
2024-07-27 01:32:10 +0800

    Update install-4.sh

--- install-4.sh
+++ install-4.sh
@@ -8,21 +8,15 @@ main() {
     echo -e "Welcome to the Solar Executor Installation Script!"
     echo -e "Install Script Version 1.0"
 
-    # Path to temporary license file
-    local license_file="/tmp/solar_executor_license.txt"
-
-    # Check license
-    echo -ne "Checking License..."
-    
     # Valid license keys
     local valid_license_keys=("AB12CD34EF56GH78IJ90KL12MN34OP56" "QR78ST90UV12WX34YZ56AB78CD90EF12" "vonmatthewmanalastas" "freekey" "GH34IJ56KL78MN90OP12QR34ST56UV78")
 
     # Prompt user for license key
-    echo -ne "\rEnter License Key:       \b\b\b\b\b\b"
-    read input_key
+    echo -ne "Enter License Key: "
+    read -r input_key
 
     # Debug output
-    echo "Debug: Entered Key = $input_key"
+    echo "Debug: Entered Key = '$input_key'"
 
     # Check if the license key is valid
     local valid=false
@@ -42,14 +36,14 @@ main() {
 
     # Proceed with installation if license key is valid
     echo -e "Downloading Solar Executor..."
-    curl -L -o SolarExecutor.zip https://github.com/bloxified/solar-executor/raw/main/Solar%20Executor.app.zip
+    curl -L -o SolarExecutor.zip "https://github.com/bloxified/solar-executor/raw/main/Solar%20Executor.app.zip"
 
     echo -n "Unzipping Solar Executor... "
     unzip -o -q SolarExecutor.zip
     echo -e "Done."
 
     echo -n "Moving Solar Executor to Applications... "
-    mv Solar\ Executor.app /Applications/
+    mv "Solar Executor.app" /Applications/
     echo -e "Done."
 
     echo -n "Cleaning up... "
@@ -57,9 +51,6 @@ main() {
     echo -e "Done."
 
     echo -e "Installation Complete! Solar Executor is now in your Applications folder."
-
-    # Clean up any temporary license file if it exists
-    rm -f "$license_file"
 }
 
 # Execute the main function
